ec013ef690499f14790d3ad5f386bd8904be2f43
lola-edy <lolaedely33@gmail.com>
2025-12-05 15:20:21 +0100

    premieres pages

---
+++ views/pages/contact.php
@@ -0,0 +1,11 @@
+<sections>
+    <h1>Contact</h1>
+    <p>Pour contacter les responsables de ce site, vous pouvez envoyer un mail à : 
+        <li>Lola EDELY : lolaedely33@gmail.com</li>
+        <li>Nathan LE FRANC : lfc.nathan33@gmail.com</li>
+        <li>Théo BOURT : bourt.theo@gmail.com</li>
+        <li>Yann POPOV : yann.popov@hotmail.fr</li></p>
+        <h1>Service</h1>
+        <p> Si vous souhaitez un rendez vous afin de trouver le design qui vous convient le mieux, planifiez un rendez-vous pour entretien dans la section <a href="rendezvous.html">Rendez-vous</a> </p>
+
+</sections>
---
+++ views/pages/legal_disclaimer.php
@@ -0,0 +1,34 @@
+<section>
+    <h1>Mentions Légales</h1>
+    <p>
+Le site internet Clartée Ornée est édité par la société :
+<br>
+EPSI<br>
+114 rue Lucien Faure<br>
+33300 Bordeaux<br>
+France<br></p>
+
+<h1>Responsables de publication</h1>
+
+<p><ul>
+<li>Lola EDELY</li>
+<li>Nathan LE FRANC</li>
+<li>Théo BOURT</li>
+<li>Yann POPOV</li>
+</ul></p>
+
+<h1>Rédaction</h1>
+<p> Le contenu de ce site à principalement été écrit par les responsables de publication : 
+<ul>
+<li>Lola EDELY</li>
+<li>Nathan LE FRANC</li>
+<li>Théo BOURT</li>
+<li>Yann POPOV</li>
+</ul></p>
+<h1>Horaires</h1>
+<p>Notre service client est ouvert du lundi au vendredi de 8h à 17h via le mail de nos responsables.
+    <li>Lola EDELY : lolaedely33@gmail.com</li>
+    <li>Nathan LE FRANC : lfc.nathan33@gmail.com</li>
+    <li>Théo BOURT : bourt.theo@gmail.com</li>        
+    <li>Yann POPOV : yann.popov@hotmail.fr</li>
+ </p></section>
