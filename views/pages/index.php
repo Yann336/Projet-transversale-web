6b73fa13319e90c28024da4b9fcf1dd81bc22a4b
Yann <yann.popov@hotmail.fr>
2025-12-08 12:07:52 +0100

    création de l'index et de la navbar

---
+++ views/pages/index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <section>
+    <h1>Calrté Ornée</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus quo sunt architecto saepe adipisci nulla deserunt nihil placeat eius earum ipsa, sint tempora corporis reprehenderit, quia eaque voluptas repellat veniam?</p>
+        <a href="#" role="button"> Nos lampes </a>
+        <a href="#" role="button"> Votre Panier </a>
+</section>
+
+
+
+<h2> Nos lampes </h2>
+
+<h3> Lampe de salon </h3>
+
+<div class= grid>
+        <article>
+            <header>
+                <img src= '#' alt='Image '>
+            </header>
+            <a href="#"> Voir plus </a>
+        </article>
+</div>
+
+<h3> Lampe de chevet </h3>
+
+<div class= grid>
+        <article>
+            <header>
+                <img src= '#' alt='Image '>
+            </header>
+            <a href="#"> Voir plus </a>
+        </article>
+</div>
+
+<h3> Lampe murale </h3>
+
+<div class= grid>
+         <article>
+            <header>
+                <img src= '#' alt='Image '>
+            </header>
+            <a href="#"> Voir plus </a>
+        </article>   
+</div>
+
+<h3> Lampe de plafond </h3>
+
+<div class= grid>
+        <article>
+            <header>
+                <img src= '#' alt='Image '>
+            </header>
+            <a href="#"> Voir plus </a>
+        </article>
+</div>
+
+
+</body>
+</html>
---
+++ views/partials/_navbar.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+    <div>
+        <nav>
+            <ul>
+                <li>
+                    <strong>
+                        <a href="index.php?page=home">Calrté Ornée</a></strong>
+                </li>
+            </ul>
+            <ul>
+                <li><a href="index.php?page=home">Accueil</a></li>
+                <li><a href="#">Nos lampes</a></li>
+                <li><a href="#">Se connecter</a></li>
+                <li><a href="#">Panier</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
+</body>
+</html>
