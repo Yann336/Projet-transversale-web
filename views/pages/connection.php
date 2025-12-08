554e8424d6a1de8163034f5e869628ef9bbc0fb5
lola-edy <lolaedely33@gmail.com>
2025-12-08 12:26:14 +0100

    nouvelles pages

---
+++ views/pages/connection.php
@@ -0,0 +1,26 @@
+<section id="form">
+    <h1>Connection</h1>
+    <form>
+        <fieldset class=grid>
+            <label>
+                Email
+                <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    autocomplete="email"/>
+            </label>
+            <label>
+            <input
+                type="password"
+                name="password"
+                placeholder="Password"
+                aria-label="Password"
+                autocomplete="current-password"/>
+            </label>
+        </fieldset>
+            <input
+			type="submit"
+			value="Se Connecter" />       
+    </form>
+</section>
