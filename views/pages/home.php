0dbc3669638d6c1fb9ffabdd2a8be262241b8dfa
Yann <yann.popov@hotmail.fr>
2025-12-09 16:52:40 +0100

    correction des classes

--- views/pages/home.php
+++ views/pages/home.php
@@ -1,4 +1,4 @@
-<section id = hero>
+<section id = 'hero'>
     <h1>Clarté Ornée</h1>
 
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus quo sunt architecto saepe adipisci nulla deserunt nihil placeat eius earum ipsa, sint tempora corporis reprehenderit, quia eaque voluptas repellat veniam?</p>
@@ -8,49 +8,49 @@
 
 
 
-<h2> Nos lampes </h2>
+<h2 class= 'lampe'> Nos lampes </h2>
 
 <h3> Lampe de salon </h3>
 
-<div class= grid>
+<div class= 'grid'>
         <article>
             <header>
                 <img src= '#' alt='Image '>
             </header>
-            <a href="#"> Voir plus </a>
+            <a href="index.php?page=lamp-details"> Voir plus </a>
         </article>
 </div>
 
 <h3> Lampe de chevet </h3>
 
-<div class= grid>
+<div class= 'grid'>
         <article>
             <header>
-                <img src= '#' alt='Image '>
+                <img src= '../../assets/images/lampe1.jpg' alt='Image'>
             </header>
-            <a href="#"> Voir plus </a>
+            <a href="index.php?page=lamp-details"> Voir plus </a>
         </article>
 </div>
 
 <h3> Lampe murale </h3>
 
-<div class= grid>
+<div class= 'grid'>
          <article>
             <header>
-                <img src= '#' alt='Image '>
+                <img src= '../../assets/images/lampe1.jpg' alt='Image'>
             </header>
-            <a href="#"> Voir plus </a>
+            <a href="index.php?page=lamp-details"> Voir plus </a>
         </article>   
 </div>
 
 <h3> Lampe de plafond </h3>
 
-<div class= grid>
+<div class= 'grid'>
         <article>
             <header>
-                <img src= '#' alt='Image '>
+                <img src= '#' alt='Image'>
             </header>
-            <a href="#"> Voir plus </a>
+            <a href="index.php?page=lamp-details"> Voir plus </a>
         </article>
 </div>
 
