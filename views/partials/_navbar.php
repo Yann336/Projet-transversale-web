02aa260963cd695c516322a76b7f37f7dd33856e
lola-edy <lolaedely33@gmail.com>
2025-12-10 10:43:28 +0100

    update

--- views/partials/_navbar.php
+++ views/partials/_navbar.php
@@ -1,18 +1,14 @@
-<header>
+<div class="page-navbar"><header>
     <div class = navbar>
         <nav>
-            <ul>
-                <li>
-                    <strong>
-                        <a href="index.php?page=home">Clarté Ornée</a></strong>
-                </li>
-            </ul>
-            <ul>
-                <li><a href="index.php?page=home">Accueil</a></li>
-                <li><a href="index.php?page=lamp-details">Nos lampes</a></li>
-                <li><a href="index.php?page=connection">Se connecter</a></li>
-                <li><a href="index.php?page=Panier">Panier</a></li>
-            </ul>
+           <strong>
+                <a href="index.php?page=home">Clarté Ornée</a></strong>
+            <a href="index.php?page=home">Accueil</a>
+            <a href="index.php?page=lamp-details">Nos lampes</a>
+            <a href="index.php?page=connection">Se connecter</a>
+            <a href="index.php?page=Panier" class="btn-panier" aria-label="Panier"> <img src="assets/images/panier.png" alt="Logo panier"></a>
+                
         </nav>
     </div>
 </header>
+</div>
