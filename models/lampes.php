427d18dc3802a545c48ea413cfd6d144541c76f3
Bourt <bourt.theo@gmail.com>
2025-12-10 08:45:05 +0100

    Nouvelle version base de données avec connexion PDO au php

--- models/lampes.php
+++ models/lampes.php
@@ -3,34 +3,26 @@
 $db = new PDO('mysql:host=localhost;dbname=Clarte_Ornee;charset=utf8mb4' , 'root', '');
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
-$sql = "SELECT * FROM LampTypes";
+$sql = "SELECT * FROM Lamps";
 $query = $db->query($sql);
-$lampTypes = $query->fetchall();
-
-$sql = "SELECT * FROM LivingRoomLamps";
-$query = $db->query($sql);
-$livingRoomLamps = $query->fetchall();
-
-
-$sql = "SELECT * FROM WallLamps";
-$query = $db->query($sql);
-$wallLamps = $query->fetchall();
-
-
-$sql = "SELECT * FROM OutdoorLamps";
-$query = $db->query($sql);
-$outdoorLamps = $query->fetchall();
-
-$sql = "SELECT * FROM LampPictures";
-$query = $db->query($sql);
-$lampPictures = $query->fetchall();
+$lamps = $query->fetchall();
 
 $sql = "SELECT * FROM Customers";
 $query = $db->query($sql);
 $customers = $query->fetchall();
 
+
+$sql = "SELECT * FROM Basket";
+$query = $db->query($sql);
+$basket = $query->fetchall();
+
+
 $sql = "SELECT * FROM Orders";
 $query = $db->query($sql);
 $orders = $query->fetchall();
+
+$sql = "SELECT * FROM Associate";
+$query = $db->query($sql);
+$associate = $query->fetchall();
 
 ?>
