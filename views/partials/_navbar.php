ade0d265c857f1d3032dcdaf08f4bdfa78f12f5c
lola-edy <lolaedely33@gmail.com>
2025-12-11 23:27:48 +0100

    update

--- views/partials/_navbar.php
+++ views/partials/_navbar.php
@@ -20,7 +20,8 @@ if (!empty($_SESSION['authenticated'])){
                 <a href="index.php?page=home">Accueil</a>
                 <a href="index.php?page=home#lampes">Nos lampes</a>
                 <a href="index.php?page=<?=$etatco?>"><?=$connect?></a>
-                <a href="index.php?page=Panier" class="btn-panier" aria-label="Panier"> <img src="assets/images/panier.png" alt="Logo panier"></a>
+                <a href="index.php?page=Panier" class="btn-panier" aria-label="Panier"> <img src="assets/images/panier.png" alt="Logo panier">
+            <span class="panier-text">Panier</span></a>
         </nav>
     </div>
  </header>
