c95343f22e0bd46852a48b01ac1ef692667790aa
lola-edy <lolaedely33@gmail.com>
2025-12-13 17:09:46 +0100

    retouches css

--- views/pages/contact.php
+++ views/pages/contact.php
@@ -1,11 +1,8 @@
-<sections>
+<section>
     <h1>Contact</h1>
     <p>Pour contacter les responsables de ce site, vous pouvez envoyer un mail à : <ul>
         <li>Lola EDELY : lolaedely33@gmail.com</li>
         <li>Nathan LE FRANC : lfc.nathan33@gmail.com</li>
         <li>Théo BOURT : bourt.theo@gmail.com</li>
         <li>Yann POPOV : yann.popov@hotmail.fr</li></ul></p>
-        <h1>Service</h1>
-        <p> Si vous souhaitez un rendez vous afin de trouver le design qui vous convient le mieux, planifiez un rendez-vous pour entretien dans la section <a href="rendezvous.html">Rendez-vous</a> </p>
-
-</sections>
+</section>
