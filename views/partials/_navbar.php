309390f5368952228a60485e474c58b6874b9337
lola-edy <lolaedely33@gmail.com>
2025-12-15 09:33:23 +0100

    change

--- views/partials/_navbar.php
+++ views/partials/_navbar.php
@@ -16,10 +16,10 @@ if (!empty($_SESSION['authenticated'])){
         <nav>
             <a href="index.php?page=home"><img class="img_logo" src="assets/images/logo_site.png" alt="Logo site"></a>
                 <strong>
-                        <a href="index.php?page=home">Clarté Ornée</a></strong>
-                <a href="index.php?page=home">Accueil</a>
-                <a href="index.php?page=home#lampes">Nos lampes</a>
-                <a href="index.php?page=<?=$etatco?>"><?=$connect?></a>
+                <a href="index.php?page=home">Clarté Ornée</a></strong>
+                <a class="lien" href="index.php?page=home">Accueil</a>
+                <a class="lien" href="index.php?page=home#lampes">Nos lampes</a>
+                <a class="lien" href="index.php?page=<?=$etatco?>"><?=$connect?></a>
                 <a href="index.php?page=Panier" class="btn-panier" aria-label="Panier"> <img src="assets/images/panier1.png" alt="Logo panier">
             <span class="panier-text">Panier</span></a>
         </nav>
