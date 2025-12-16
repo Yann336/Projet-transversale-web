7e1b552b14e0039a5790e202a6a171e1658c420d
Boblamerguez <lfc.nathan33@gmail.com>
2025-12-17 00:31:56 +0100

    dernière modif

--- views/pages/achat.php
+++ views/pages/achat.php
@@ -1,11 +1,11 @@
-<?php 
+<?php
 
 include('dbConnect.php');
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['pay'])) {
 
 
-    $email = $_SESSION['pseudo']; 
+    $email = $_SESSION['pseudo'];
 
     $sql = "SELECT idCustomer FROM customers WHERE Email = :email LIMIT 1";
     $stmt = $db->prepare($sql);
@@ -14,7 +14,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['pay'])) {
 
     if ($customer) {
 
-  
+
         $sql = "DELETE FROM baskets WHERE idCustomer = :idCustomer";
         $stmt = $db->prepare($sql);
         $stmt->execute([
@@ -28,55 +28,73 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['pay'])) {
 
 ?>
 <section>
-<div class="containers">
-        <form method="post" action="index.php?page=achat">
-        <div class="delivery_method">
-          <h4>méthode d'expedition</h4>
+    <!DOCTYPE html>
+    <html lang="en">
 
-         <label for="Street">Adresse :</label><br>
-                <input type="text" id="Street" name="Street" required><br><br>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <link rel="stylesheet" href="../../assets/css/achat.css">
+    </head>
 
-                <label for="City">Ville :</label><br>
-                <input type="text" id="City" name="City" required><br><br>
+    <body>
+        <div class="container">
+            <form action="#">
 
-                <label for="Country">Pays :</label><br>
-                <input type="text" id="Country" name="Country" required><br><br>
+                <div class="delivery_method">
+                    <h4>méthode d'expedition</h4>
 
-                <label for="PostCode">Code postal :</label><br>
-                <input type="text" id="PostCode" name="PostCode"
-                    inputmode="numeric" placeholder="12345" pattern="[0-9]{5}" maxlength="5" required><br><br>
+                    <label for="Street">Adresse :</label>
+                    <input type="text" id="Street" name="Street" required>
 
-                <input type="radio" id="dpd" name="dpd" value="dpd">
-                <label for="dpd">€9,95 Livré par DPD sous 2 à 4 jours ouvrables</label>
-            </div>
+                    <label for="City">Ville :</label>
+                    <input type="text" id="City" name="City" required>
 
-            <div class="payment_column">
-                <div class="payment_method">
-                    <h4>méthode de payement</h4>
-                    <label for="NumberCard">Numéro de carte :</label><br>
-                    <input type="text" id="NumberCard" name="NumberCard"
-                        inputmode="numeric" placeholder="1234567890123456" pattern="[0-9]{16}" maxlength="16" required><br><br>
+                    <label for="Country">Pays :</label>
+                    <input type="text" id="Country" name="Country" required>
 
-                    <label for="ExperyDate">Date d'expiration :</label><br>
-                    <input type="text" id="ExperyDate" name="ExperyDate"
-                        inputmode="numeric" placeholder="MM/AA" pattern="(0[1-9]|1[0-2])\/[0-9]{2}" required><br><br>
+                    <label for="PostCode">Code postal :</label>
+                    <input type="text" id="PostCode" name="PostCode"
+                        inputmode="numeric" placeholder="12345" pattern="[0-9]{5}" maxlength="5" required>
 
-                    <label for="SecurityCode">Code de sécurité :</label><br>
-                    <input type="text" id="SecurityCode" name="SecurityCode"
-                        inputmode="numeric" placeholder="123" pattern="[0-9]{3}" maxlength="3" required><br><br>
-
-                    <label for="NameCard">Nom du titulaire :</label><br>
-                    <input type="text" placeholder="Joe Dohn" id="NameCard" name="NameCard" required><br><br>
+                    <input type="radio" id="dpd" name="dpd" value="dpd">
+                    <label class="dpd" for="dpd">€9,95 Livré par DPD sous 2 à 4 jours ouvrables</label>
                 </div>
 
-                <div class="payment">
-                    <h4><strong>passez au reglement</strong></h4>
-                    <a href="">voici le contenue de votre panier</a> <br> <br>
-                    <input class="submit_button" type="submit" value="envoyer">
+                <div class="payment_column">
+
+                    <div class="payment_method">
+                        <h4>méthode de payement</h4>
+
+                        <label for="NumberCard">Numéro de carte :</label>
+                        <input type="text" id="NumberCard" name="NumberCard"
+                            inputmode="numeric" placeholder="1234567890123456" pattern="[0-9]{16}" maxlength="16" required>
+
+                        <label for="ExperyDate">Date d'expiration :</label>
+                        <input type="text" id="ExperyDate" name="ExperyDate"
+                            inputmode="numeric" placeholder="MM/AA" pattern="(0[1-9]|1[0-2])/[0-9]{2}" required>
+
+                        <label for="SecurityCode">Code de sécurité :</label>
+                        <input type="text" id="SecurityCode" name="SecurityCode"
+                            inputmode="numeric" placeholder="123" pattern="[0-9]{3}" maxlength="3" required>
+
+                        <label for="NameCard">Nom du titulaire :</label>
+                        <input type="text" placeholder="Joe Dohn" id="NameCard" name="NameCard" required>
+                    </div>
+
+                    <div class="payment">
+                        <h4><strong>passez au reglement</strong></h4>
+                        <a href="">voici le contenue de votre panier</a>
+                        <input class="submit_button" type="submit" value="envoyer">
+                    </div>
+
                 </div>
-            </div>
-        </form>
-    </div>
-</section>
+
+            </form>
+        </div>
+
+    </body>
 
 
+    </html>
